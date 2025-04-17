18b65c8a1cebcb1208c7eac54d15f0dac763950b
SamueldeJeusdeOliveira <samuksjj@gmail.com>
2025-04-17 20:53:22 -0300

    Adicionar arquivos de projeto.

---
+++ RoboTupiniquim/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace RoboTupiniquim
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Robo robo1 = new Robo();
+
+            string posicaoInicial = "3 3 L";
+            string comandos = "MMDMMDMDDM";
+
+            string[] coordenadasIniciais = posicaoInicial.Split(' ');
+            Robo.PosicaoX = Convert.ToInt32(coordenadasIniciais[0]);
+            Robo.PosicaoY = Convert.ToInt32(coordenadasIniciais[1]);
+            Robo.Direcao = coordenadasIniciais[2][0];
+
+            Robo.ExibirCoordenadas();
+            Robo robo2 = new Robo();
+            string posicaoInicial2 = "4 1 O";
+            string comandos2 = "MMDMMDMDDM";
+
+            string[] coordenadasIniciais2 = posicaoInicial2.Split(' ');
+            Robo.PosicaoX = Convert.ToInt32(coordenadasIniciais2[0]);
+            Robo.PosicaoY = Convert.ToInt32(coordenadasIniciais2[1]);
+            Robo.Direcao = coordenadasIniciais2[2][0];
+            Robo.Mover(comandos2);
+
+            Robo.ExibirCoordenadas();
+        }
+    }
+}
---
+++ RoboTupiniquim/Robo.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoboTupiniquim
+{
+    class Robo
+    {
+        public static int PosicaoX;
+        public static int PosicaoY;
+        public static char Direcao;
+
+        public static void Mover(string comandos)
+        {
+            foreach (char comando in comandos)
+            {
+                if (comando == 'M')
+                {
+                    MoverParaFrente();
+                }
+                else if (comando == 'D')
+                {
+                    VirarDireita();
+                }
+                else if (comando == 'E')
+                {
+                    VirarEsquerda();
+                }
+            }
+        }
+            public static void MoverParaFrente()
+        {
+            switch (Direcao)
+            {
+                case 'N':
+                    PosicaoY++;
+                    break;
+                case 'L':
+                    PosicaoX++;
+                    break;
+                case 'S':
+                    PosicaoY--;
+                    break;
+                case 'O':
+                    PosicaoX--;
+                    break;
+            }
+        }
+
+        public static void VirarDireita()
+        {
+            switch (Direcao)
+            {
+                case 'N':
+                    Direcao = 'L';
+                    break;
+                case 'L':
+                    Direcao = 'S';
+                    break;
+                case 'S':
+                    Direcao = 'O';
+                    break;
+                case 'O':
+                    Direcao = 'N';
+                    break;
+            }
+        }
+
+        public static void VirarEsquerda()
+        {
+            switch (Direcao)
+            {
+                case 'N':
+                    Direcao = 'O';
+                    break;
+                case 'L':
+                    Direcao = 'N';
+                    break;
+                case 'S':
+                    Direcao = 'L';
+                    break;
+                case 'O':
+                    Direcao = 'S';
+                    break;
+            }
+        }
+
+        public static void ExibirCoordenadas()
+        {
+            Console.WriteLine($"Posição final: {PosicaoX} {PosicaoY} {Direcao}");
+        }
+    }
+}
